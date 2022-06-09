ff75144e4b6af01e6912f32191772372f3069452
aselwilliams <aselwilliams22@gmail.com>
2022-06-09 17:12:53 -0400

    new component, data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-
+import Question from './Question';
 import './App.css';
+import data from './data'
 
 function App() {
+  const [questions, setQuestions] =useState(data)
   return (
-    <div className="App">
-    
+    <div className="container">
+      <header>
+        <h1>Questons and answers about login</h1>
+      </header>
+      <section>
+        {questions.map((question)=>{
+          return (
+            <Question key={question.id} {...question} />
+          )
+        })}
+      </section>    
     </div>
   );
 }
